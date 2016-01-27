3bff705d15602ba3090d2a2aa4b01b53ffbdfaf6
David256 <davidborja456@gmail.com>
2016-01-27 13:53:27 -0500

    documentación de clase Panel

--- src/interfaz/Panel.java
+++ src/interfaz/Panel.java
@@ -2,6 +2,13 @@ package interfaz;
 
 import javax.swing.JPanel;
 
-public class Panel extends JPanel{
+/**
+ * Clase Panel heredada de la clase JPanel.
+ * 
+ * @author David Elias
+ * @see javax.swing.JPanel
+ *
+ */
+public class Panel extends JPanel {
 
 }
