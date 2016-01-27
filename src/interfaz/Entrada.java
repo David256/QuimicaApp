38e226f6298375a3017232b062982917a7ae882f
David256 <davidborja456@gmail.com>
2016-01-27 13:48:32 -0500

    documentación de clase Entrada, campo de texto

--- src/interfaz/Entrada.java
+++ src/interfaz/Entrada.java
@@ -2,12 +2,32 @@ package interfaz;
 
 import javax.swing.JTextField;
 
-public class Entrada extends JTextField{
-	
-	public Entrada(String valor){
+/**
+ * Clase Entrada hereda de la clase JTextField y permite la creación de un campo
+ * de entrada de texto.
+ * 
+ * @author David Elias
+ * @version 1.0
+ * @see javax.swing.JTextField
+ * @see "Un buen libro de programación en Java"
+ */
+public class Entrada extends JTextField {
+
+	/**
+	 * Contructor que recibe un valor que mostrará en el campo de texto
+	 * 
+	 * @param valor
+	 *            Variable String que define el texto que se mostrará en el
+	 *            campo de texto
+	 */
+	public Entrada(String valor) {
 		super(valor);
 	}
-	public Entrada(){
+
+	/**
+	 * Constructor que no necesita recibir parámetro
+	 */
+	public Entrada() {
 		super("");
 	}
 
