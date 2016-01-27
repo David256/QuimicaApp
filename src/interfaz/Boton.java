f87deed84ee2b1618fd15e9cce0571176418a1d5
David256 <davidborja456@gmail.com>
2016-01-27 13:36:10 -0500

    documentación de clase Boton

--- src/interfaz/Boton.java
+++ src/interfaz/Boton.java
@@ -5,18 +5,41 @@ import java.awt.Font;
 
 import javax.swing.JButton;
 
-public class Boton extends JButton{
-	
-	public Boton(String valor){
+/**
+ * Esta clase define a un botón de forma particular
+ * 
+ * @author David Elias
+ * @version 1.5
+ */
+public class Boton extends JButton {
+
+	/**
+	 * Contructor de la clase.
+	 * 
+	 * @param valor
+	 *            Variable de tipo String que define el texto del botón.
+	 */
+	public Boton(String valor) {
 		super(valor);
+		// ahora llamamos al método que nos ayudará un montón
 		hacerBonito();
 	}
-	public Boton(){
+
+	/**
+	 * Contructor de la clase, pero esta no tiene parametro. En este caso es
+	 * vacio
+	 */
+	public Boton() {
 		super("");
+		// ahora llamamos al método que nos ayudará un montón
 		hacerBonito();
 	}
-	
-	public void hacerBonito(){
+
+	/**
+	 * Método que formatea el objeto: le coloca color de fondo y a la letra,
+	 * también define una tipográfica y su respectivo estilo.
+	 */
+	public void hacerBonito() {
 		this.setBackground(Color.WHITE);
 		this.setForeground(Color.blue);
 		this.setFont(new Font("Arial", Font.BOLD, 14));
