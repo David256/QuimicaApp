09275ff9cc133a09b661c7716acfbb03fecb582a
David256 <davidborja456@gmail.com>
2016-01-27 13:51:45 -0500

    documentación de clase Etiqueta

--- src/interfaz/Etiqueta.java
+++ src/interfaz/Etiqueta.java
@@ -2,12 +2,31 @@ package interfaz;
 
 import javax.swing.JLabel;
 
-public class Etiqueta extends JLabel{
-	
-	public Etiqueta(String valor){
+/**
+ * Clase que hereda de JLabel, y permite crear etiquetas
+ * 
+ * @author David Elias
+ * @version 1.1
+ * @see javax.swing.JLabel
+ */
+public class Etiqueta extends JLabel {
+
+	/**
+	 * Contructor de la clase, la cual necesita un valor para mostrar en la
+	 * etiqueta.
+	 * 
+	 * @param valor
+	 *            Este será el valor que es mostrará
+	 */
+	public Etiqueta(String valor) {
 		super(valor);
 	}
-	public Etiqueta(){
+
+	/**
+	 * Contructor de la clase, el cual crea una etiqueta vacia, o sea, sin
+	 * texto.
+	 */
+	public Etiqueta() {
 		super("");
 	}
 
